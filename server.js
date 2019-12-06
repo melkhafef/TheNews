e5deb0623d6039b70046890116a386b11b43e5ed
mohamedElkhafef <melkhafef@yahoo.com>
2019-12-06 20:56:27 +0200

    delete console

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-console.log(data);
 app.use(express.static(__dirname + '/dist/theNews'));
 app.use(cors());
 app.get('/', (req, res) => {
